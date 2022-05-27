8677e4f76897b9f66534252fa44afb17b9c580ea
yjh4218 <yjh0950@gmail.com>
2022-05-27 17:57:20 +0900

    Controller -RestController 수정

--- src/main/java/com/vue_spring/demo/controller/ProductController.java
+++ src/main/java/com/vue_spring/demo/controller/ProductController.java
@@ -8,22 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @GetMapping("/selectProducts")
-    public String list(Model model) {
+    public void list(Model model) {
         System.out.println("Controller 접근됨");
         List<Product> products = productService.findProductAll();
         System.out.println("Service 조회 완료");
         // System.out.println(products);
         model.addAttribute("product", products);
         System.out.println(model);
-
-        return "111";
     }
 }
